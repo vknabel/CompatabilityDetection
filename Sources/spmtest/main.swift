6b3223bfc7c11b9542db3bed832d2ba0220bf8a1
Valentin Knabel <dev@vknabel.com>
2018-06-06 17:04:46 +0200

    Updated dependencies

--- Sources/spmtest/main.swift
+++ Sources/spmtest/main.swift
@@ -4,6 +4,6 @@ import CompatabilityDetection
 let diffUrl = ProcessInfo.processInfo.arguments[1]
 let results = compatabilityTestAdditionsForUrl(diffUrl)
 
-for (repo, error) in results {
-    fail("\(repo.name) seems to be incompatible with \(osName): \(error)")
+for (repo, error) in results ?? [] {
+    print("\(repo.name) seems to be incompatible: \(error)")
 }
